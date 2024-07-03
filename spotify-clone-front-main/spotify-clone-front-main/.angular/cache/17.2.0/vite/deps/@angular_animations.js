6159b674c5df166fcc74374b2bf8916a5881e070
neerajrapelli <neerajrapelli@gmail.com>
2024-07-03 13:56:42 -0400

    first commit

---
+++ spotify-clone-front-main/spotify-clone-front-main/.angular/cache/17.2.0/vite/deps/@angular_animations.js
@@ -0,0 +1,51 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-MN5H3HXW.js";
+import "./chunk-NHJGMXCN.js";
+import "./chunk-KXXOWBCK.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-WKYGNSYM.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
---
+++ spotify-clone-front-main/spotify-clone-front-main/README.md
@@ -0,0 +1,23 @@
+### Key Features :
+- 🔐 Security with Auth0 (OAuth2)
+- 🎶➕ Music Upload
+- 🎧 Audio Streaming
+- 🔍 Search Function
+- ❤️ User Favorites
+- 📱💻 Responsive UI with Bootstrap
+- 🅰️ Latest Angular Features: Signal, Standalone Component, New Control Flow Syntax
+
+## Usage
+### Prerequisites
+- [NodeJS 20.11 LTS](https://nodejs.org/dist/v20.11.1/node-v20.11.1.pkg)
+- [Angular CLI v17](https://www.npmjs.com/package/@angular/cli)
+- IDE (VSCode, IntelliJ, Webstorm)
+
+### Fetch dependencies
+``npm install``
+
+### Launch dev server
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+### Build
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
